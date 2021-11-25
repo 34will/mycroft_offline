7aa7c5cdbd55275a735febcac8a3fe3361eeb6fd
34will <wgilmour@hotmail.co.uk>
2021-11-25 01:18:19 +0000

    Split out selene-backend Dockerfile into selene-backend and selene-db-setup.

--- selene-db-setup/wait-for-postgres.sh
+++ selene-db-setup/wait-for-postgres.sh
@@ -4,9 +4,10 @@
 set -e
 
 host="$1"
+port="$1"
 shift
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "postgres" -c '\q'; do
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -p "$port" -U "postgres" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
