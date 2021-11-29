de978c16dfdfa446b5d5a72e8c94408b5630a89f
34will <wgilmour@hotmail.co.uk>
2021-11-29 03:48:40 +0000

    Added selene-ui Dockerfile

---
+++ selene-ui/replace-domain.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# replace-domain.sh
+
+find /usr/share/nginx/html -type f -exec sed -i -e "s/account\.mycroft\.ai/$1/g" {} \;
+find /usr/share/nginx/html -type f -exec sed -i -e "s/market\.mycroft\.ai/$2/g" {} \;
+find /usr/share/nginx/html -type f -exec sed -i -e "s/sso\.mycroft\.ai/$3/g" {} \;
