3acb414d69b2921e6c40fb93f0b9effc6f8bc54b
34will <wgilmour@hotmail.co.uk>
2021-11-30 01:30:10 +0000

    Modified replace-domain.sh to replace home.mycroft.ai as well

--- selene-ui/replace-domain.sh
+++ selene-ui/replace-domain.sh
@@ -2,5 +2,6 @@
 # replace-domain.sh
 
 find /usr/share/nginx/html -type f -exec sed -i -e "s/account\.mycroft\.ai/$1/g" {} \;
+find /usr/share/nginx/html -type f -exec sed -i -e "s/home\.mycroft\.ai/$1/g" {} \;
 find /usr/share/nginx/html -type f -exec sed -i -e "s/market\.mycroft\.ai/$2/g" {} \;
 find /usr/share/nginx/html -type f -exec sed -i -e "s/sso\.mycroft\.ai/$3/g" {} \;
